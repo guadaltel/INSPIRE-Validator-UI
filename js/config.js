8925020869e92e54785d3f5e72cdd6d3cfd37169
josebarrero <josebarrero@josebarrero>
2022-03-16 16:25:56 +0100

    Changes for release 2022.1 (production environment)

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 var captchaEnabled = true;
 var betaBanner = false;
-var labelStaging = true;
-var environment = "STAGING";
-// STAGING
-var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
-var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
-var serverDirectURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
-var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/captcha/verify.php";
-var serverToken = "";
+var labelStaging = false;
+var environment = "PROD";
+// PROD
+var serverURL = "https://inspire.ec.europa.eu/validator/v2/";
+var serverRealURL = "https://inspire.ec.europa.eu/validator/v2/";
+var serverDirectURL = "https://yzyiqfakm4.execute-api.eu-west-1.amazonaws.com/validator/v2/";
+var serverCaptchaURL = "https://inspire.ec.europa.eu/validator/captcha/verify.php";
+var serverToken = "BQIAJulY249NyeiT6gsfc9AUTCa8Nkgi72J9jIVP";

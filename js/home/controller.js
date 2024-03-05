3b357ac9f4b008fac3de70cdb26b5f561b2a4502
Daniel Martín Pérez de León <114756164+DanielMartinPerezdeLeon@users.noreply.github.com>
2024-03-05 13:15:31 +0100

    Revert "Added VALIDATOR version as var and introduced as comment in home head"

--- js/home/controller.js
+++ js/home/controller.js
@@ -9,13 +9,3 @@ if (betaBanner == true) {
 if (labelStaging == true) {
 	$(document).prop('title', "[STAGING] " + $(document).prop('title'));
 }
-
-//Show version label if exists
-if (validatorVersionLabel !== null && validatorVersionLabel !== undefined) {
-
-	// Create a comment with the version label
-	var commentNode = document.createComment('Validator version: ' + validatorVersionLabel);
-
-	// Insert the comment node right at the start of the head
-	document.head.insertBefore(commentNode, document.head.firstChild);
-}
